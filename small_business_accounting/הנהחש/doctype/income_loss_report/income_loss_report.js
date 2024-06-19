9af9310828337c3133f516cc1bc9b401392a9f09
Alon Ben Refael <alonbr@proton.me>
2024-06-19 12:26:05 +0300

    Update income_loss_report.js

--- small_business_accounting/הנהחש/doctype/income_loss_report/income_loss_report.js
+++ small_business_accounting/הנהחש/doctype/income_loss_report/income_loss_report.js
@@ -1,103 +1,60 @@
 // Copyright (c) 2024, Alon Ben Refael and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Income Loss Report", {
-// 	refresh(frm) {
 
-// 	},
-// });
 
 
 frappe.ui.form.on('Income Loss Report', {
 	calc(frm) {
-		var total_profit = 0;
-		var asset_loss = 0;
-		var losses = 0;
-		var profit_pre;
-		var profit_adj;
-		var car_non = 0;
-	    function finale(){
-			frm.set_value('total_profit',total_profit);
-			frm.set_value('asset_loss',asset_loss);
-			refresh_field("total_profit");
-			refresh_field("asset_loss");
-			profit_pre = total_profit - losses - asset_loss;
-			frm.set_value('profit_pre', profit_pre);
-		    refresh_field("profit_pre");
-			frm.set_value('car_non', car_non);
-			refresh_field('car_non');
-			profit_adj = profit_pre + car_non;
-			frm.set_value('profit_adj', profit_adj);
-		    refresh_field('profit_adj');
-		    frm.save();
-	    }
-		frm.save();
-		var year = frm.doc.year;
-		var rec_dict={};
-		frappe.db.get_list('Receipt',{
-			filters:{
-				'receipt_date':['between', year+'-01-01', year+'-12-31'],
-				'caceled':0
-			},
-			fields:['most_impact', 'total']
-		}).then(records => {
-			records.forEach((item) => {
-				let itm = item.most_impact;
-				if (itm in rec_dict){
-					rec_dict[itm] = rec_dict[itm] + item.total;
-				} else {
-					rec_dict[itm] = item.total;
-				}
-				total_profit = total_profit + item.total;
-			});
-			var row;
-			for (const key of Object.keys(rec_dict)) {
-				row = frm.add_child("items");
-				row.item = key;
-				row.sum = rec_dict[key];
-				refresh_field("items");
+		frappe.call({
+			method: 'small_business_accounting.%D7%94%D7%A0%D7%94%D7%97%D7%A9.doctype.income_loss_report.income_loss_report.get_data',
+			args: {
+				"fisc_year": frm.doc.year
 			}
-			frappe.db.get_list('Assets',{
-				filters:{
-					'fiscal_year':year
-				},
-				fields:['loss_requested']
-			}).then(records => {
-					records.forEach((item) => {
-						asset_loss = asset_loss + item.loss_requested;
-					});
-					frappe.db.get_list('Expenses',{
-						filters:{
-							'when':['between', year+'-01-01', year+'-12-31']
-						},
-						fields:['type', 'sum', 'actual_sum']
-					}).then(records => {
-						records.forEach((item) => {
-							let actual_sum = item.actual_sum;
-							let typ = item.type;
-							let val = item.sum;
-							if (typ == 'הוצאות רכב'){
-								car_non = car_non + val - actual_sum;
-								frm.set_value('car',frm.doc.car + val);
-								refresh_field('car');
-								losses = losses + val;
-							} else if (typ == 'ביטוח מקצועי והשתלמויות'){
-								frm.set_value('insurance',frm.doc.insurance + actual_sum);
-								refresh_field('insurance');
-								losses = losses + actual_sum;
-							} else if (typ == 'משרדיות ואחזקה'){
-								frm.set_value('office',frm.doc.office + actual_sum);
-								refresh_field('office');
-								losses = losses + actual_sum;
-							} else if (typ == 'קבלני משנה'){
-								frm.set_value('subconturctors',frm.doc.subconturctors + actual_sum);
-								refresh_field('subconturctors');
-								losses = losses + actual_sum;
-							}
-						});
-						finale();
-					});
-				});
+    	}).then(r => {
+			const EXPENSES = r.message[1];
+			const asset_loss = r.message[2];
+			const RECEIPT = r.message[3];
+			const CAR_NON = r.message[0];
+			var losses = 0;
+			var total_income = 0;
+			var car = 0;
+			var insurance = 0;
+			var office = 0;
+			var subconturctors = 0;
+			for (const key of Object.keys(EXPENSES)) {
+				let val = EXPENSES[key];
+				if (key == 'הוצאות רכב'){
+					car = val;
+				} else if (key == 'ביטוח מקצועי והשתלמויות'){
+					insurance = val;
+				} else if (key == 'משרדיות ואחזקה'){
+					office = val;
+				} else if (key == 'קבלני משנה'){
+					subconturctors = val;
+				}
+				losses += val;
+			}
+			for (const key of Object.keys(RECEIPT)) {
+				let row = frm.add_child("items");
+				let val = RECEIPT[key];
+				row.item = key;
+				row.sum = val;
+				refresh_field("items");
+				total_income += val;
+			}
+			var net_non = total_income - losses - asset_loss;
+			frm.set_value('asset_loss', asset_loss);
+			frm.set_value('subconturctors',subconturctors);
+			frm.set_value('office',office);
+			frm.set_value('insurance',insurance);
+			frm.set_value('car',car);
+			frm.set_value('car_non', CAR_NON);
+			frm.set_value('total_profit', total_income);
+			frm.set_value('profit_pre', net_non);
+			frm.set_value('profit_adj', total_income + net_non);
+			frm.refresh();
+			frm.save();
 		});
 	}
 });
