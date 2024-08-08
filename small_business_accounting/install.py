d80a8f8bb1f8bfd085bb046da7137520103a8e2d
Alon Ben Refael <alonbr@proton.me>
2024-08-08 09:47:22 +0300

    Update install.py

--- small_business_accounting/install.py
+++ small_business_accounting/install.py
@@ -6,12 +6,12 @@ def after_install():
 	if not file_exists:
 		import shutil, os, csv
 		shutil.copytree('/home/frappe/frappe-bench/apps/small_business_accounting/apps', '/home/frappe/apps')
-		# file = open('/home/frappe/apps/asset_type_list.csv','r')
-		# reader = csv.reader(file, delimiter=',')
-		# for row in reader:
-			# doc = frappe.new_doc('Asset Type List')
-			# type = row[0]
-			# doc.name = type
-			# doc.asset_type = type
-			# doc.percent = float(row[1])
-			# doc.insert()
+		file = open('/home/frappe/apps/asset_type_list.csv','r')
+		reader = csv.reader(file, delimiter=',')
+		for row in reader:
+			doc = frappe.new_doc('Asset Type List')
+			type = row[0]
+			doc.name = type
+			doc.asset_type = type
+			doc.percent = float(row[1])
+			doc.insert()
