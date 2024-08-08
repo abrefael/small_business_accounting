56636f5641fb419993b5a3f5dd8b32573aff136d
Alon Ben Refael <alonbr@proton.me>
2024-08-08 10:40:01 +0300

    Update asset_type_list.js

--- small_business_accounting/הנהחש/doctype/asset_type_list/asset_type_list.js
+++ small_business_accounting/הנהחש/doctype/asset_type_list/asset_type_list.js
@@ -8,7 +8,7 @@
 // });
 frappe.ui.form.on('Asset Type List', {
 	additional_information(frm) {
-            const URL = 'https://mo.ralc.co.il/%D7%9E%D7%90%D7%9E%D7%A8-1115,1987-%D7%A9%D7%99%D7%A2%D7%95%D7%A8%D7%99-%D7%94%D7%A4%D7%97%D7%AA-%D7%94%D7%A9%D7%A0%D7%AA%D7%99-%D7%9C%D7%A4%D7%99-%D7%AA%D7%A7%D7%A0%D7%95%D7%AA-%D7%9E%D7%A1-%D7%94%D7%9B%D7%A0%D7%A1%D7%94-%D7%A4%D7%97%D7%AA-1941.aspx';
+            const URL = 'https://www.nevo.co.il/law_html/law01/255_001.htm';
             window.open(URL, '_blank').focus();
 	}
 });
