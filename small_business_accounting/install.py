e0a89f4f98b64aeb72ffd91994b3bbcafec09c7a
Alon Ben Refael <alonbr@proton.me>
2024-08-08 11:41:54 +0300

    Update install.py

--- small_business_accounting/install.py
+++ small_business_accounting/install.py
@@ -1,17 +1,41 @@
+src_fldr = '/home/frappe/frappe-bench/apps/small_business_accounting/apps'
+dest_fldr = '/home/frappe/apps'
+csv_file = '/asset_type_list.csv'
+
 import frappe
+import shutil, os, csv
+
 def after_install():
-	from os.path import exists
-	flag = '/home/frappe/apps/Explanation'
-	file_exists = exists(flag)
-	if not file_exists:
-		import shutil, os, csv
-		shutil.copytree('/home/frappe/frappe-bench/apps/small_business_accounting/apps', '/home/frappe/apps')
-		file = open('/home/frappe/apps/asset_type_list.csv','r')
+	shutil.copytree(src_fldr, dest_fldr)
+	file = open(dest_fldr + csv_file,'r')
+	reader = csv.reader(file, delimiter=',')
+	for row in reader:
+		doc = frappe.new_doc('Asset Type List')
+		type = row[0]
+		doc.name = type
+		doc.asset_type = type
+		doc.percent = float(row[1])
+		doc.insert()
+
+
+def after_migrate():
+	import filecmp
+	if not filecmp.cmp(src_fldr + csv_file, dest_fldr + csv_file):
+		shutil.copyfileobj(src_fldr + csv_file, dest_fldr + csv_file)
+		file = open(dest_fldr + csv_file,'r')
 		reader = csv.reader(file, delimiter=',')
 		for row in reader:
-			doc = frappe.new_doc('Asset Type List')
 			type = row[0]
-			doc.name = type
-			doc.asset_type = type
-			doc.percent = float(row[1])
-			doc.insert()
+			percent = float(row[1])
+			try:
+				doc = frappe.get_doc('Asset Type List', type)
+			except:
+				doc = frappe.new_doc('Asset Type List')
+				doc.name = type
+				doc.asset_type = type
+				doc.percent = percent
+				doc.insert()
+			else:
+				if not percent == doc.percent:
+					doc.percent = percent
+					doc.save()
