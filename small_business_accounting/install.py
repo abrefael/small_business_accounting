e330ea2d97c110f45fca133f835a8b2bcf727393
Alon Ben Refael <alonbr@proton.me>
2024-08-19 08:31:07 +0300

    Update install.py

--- small_business_accounting/install.py
+++ small_business_accounting/install.py
@@ -9,7 +9,7 @@ def after_install():
 	from getpass import getpass
 	import subprocess
 	password = getpass('Please enter sudo password: ')
-	shutil.copytree(src_fldr, dest_fldr)
+	os.system("cp -rf '" + src_fldr + "' '" + dest_fldr + "'")
 	subprocess.call('echo ' + password + ' | sudo -S apt install libreoffice-writer-nogui -y', shell=True)
 	file = open(dest_fldr + csv_file,'r')
 	reader = csv.reader(file, delimiter=',')
