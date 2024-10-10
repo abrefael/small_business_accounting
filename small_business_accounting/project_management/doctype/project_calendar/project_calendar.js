68a82bd591ffd2a67813e2aa3ad0b58091b769fb
Alon Ben Refael <alonbr@proton.me>
2024-10-10 19:34:34 +0300

    Create project_calendar.js

---
+++ small_business_accounting/project_management/doctype/project_calendar/project_calendar.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2024, Alon Ben Refael and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sessions Calendar", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+
+frappe.ui.form.on('Project Calendar', {
+  refresh(frm) {
+
+ 	}
+});
