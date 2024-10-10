d9af31530540ff2ec49788c74a8fac52079a41ec
Alon Ben Refael <alonbr@proton.me>
2024-10-10 19:24:46 +0300

    Update install.py

--- small_business_accounting/install.py
+++ small_business_accounting/install.py
@@ -23,6 +23,21 @@ def after_install():
 
 def after_migrate():
 	import filecmp
+	frappe.db.sql("""
+	INSERT
+	INTO `tabCustom HTML Block`
+	SET
+		name='Project_Calendar',
+		creation=NOW(),
+		modified=NOW(),
+		modified_by='Administrator',
+		owner='Administrator',
+		docstatus=0,
+		private=0,
+		html='<iframe src="/app/sessions-calendar/view/calendar" style="width:100%;height: 850px;"></iframe>',
+		script='',
+		style='';
+	""")
 	if not filecmp.cmp(src_fldr + csv_file, dest_fldr + csv_file):
 		os.system("cp -rf '" + src_fldr + csv_file + "' '" + dest_fldr + csv_file + "'")
 		file = open(dest_fldr + csv_file,'r')
