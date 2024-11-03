b2cdd843efcf055ad48765fc9e05a1fe6da931c9
Alon Ben Refael <alonbr@proton.me>
2024-11-03 10:40:33 +0200

    Update sales.py

--- small_business_accounting/הנהחש/doctype/sales/sales.py
+++ small_business_accounting/הנהחש/doctype/sales/sales.py
@@ -77,7 +77,7 @@ def Create_Quotation(q_num, objective, notes):
 	body.append(title1)
 	title1 = Header(2, f"עבור: {doc.client}")
 	body.append(title1)
-	title1 = Header(2, f"ע.מ/ת.ז: {doc.h_p}")
+	title1 = Header(2, f"ע.מ/ת.ז/\"ר: {doc.h_p}")
 	body.append(title1)
 	body.append(Paragraph(""))
 	body.append(Paragraph(""))
